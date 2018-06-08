b8e43ef37f953d1d51c8f465beb6dd3c1767379b
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2018-06-08 17:39:15 +0200

    setup hello branch

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+/**
+ * \brief minimal Hello program (doxygen syntax)
+ * \author Sebastien COUDERT
+**/
+
+#include <string>         // std::string
+#include <iostream>       // std::cout
+
+//! hello starts here
+int main(int argc, char **argv)
+{
+  //! print hello message on terminal
+  std::cout<<"Hello C++"<<std::endl;
+  return 0;
+}//main
+
